f7768dce195e074d267a4170374efc47ef7ce881
Yogaraj <yogarajjuju22@gmail.com>
2025-06-20 10:44:16 +0530

    bankai

---
+++ src/OddorEven.java
@@ -0,0 +1,11 @@
+public class OddorEven {
+    public static void main(String[] args) {
+        int n = 4;
+        if((n&1)==1){
+            System.out.println("odd");
+
+        } else{
+            System.out.println("even");
+        }
+    }
+}
