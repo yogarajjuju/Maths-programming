03324567a5d5588700247dd12177d973284bd9ab
Yogaraj <yogarajjuju22@gmail.com>
2025-06-21 14:32:55 +0530

    bankai

--- src/Power.java
+++ src/Power.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Power {
+    public static void main(String[] args) {
+        int base =9;
+        int power = 2;
+        int ans =1;
+        while(power>0){
+            if((power&1)==1){
+                ans*=base;
+            }
+            base*=base;
+            power=power>>1;
+        }
+        System.out.println(ans);
+    }
 }
