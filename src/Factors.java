a05cd3bee8d3484241192c89caeb5391e62d981a
Yogaraj <yogarajjuju22@gmail.com>
2025-06-23 15:02:25 +0530

    bankai

--- src/Factors.java
+++ src/Factors.java
@@ -1,7 +1,7 @@
 public class Factors {
     public static void main(String[] args) {
         int n = 20;
-        for(int i =1; i<=n;i++){
+        for(int i =1; i<=n;i++){//bryte force
             if(n%i==0){
                 System.out.println(i);
             }
